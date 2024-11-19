6dd643eb1a2992bf3408a49dd7ec80d83e0da5ed
skyoungbin <skyoungbin@naver.com>
2024-11-19 19:14:59 +0900

    fix: module err

--- myapp/test/test_import.py
+++ myapp/test/test_import.py
@@ -5,6 +5,8 @@ def test_imports():
         import requests  # REST API 요청을 위한 라이브러리
         import zmq
         import zmq.asyncio 
+        import websockets
+        import aiohttp
         assert True
     except ImportError as e:
         pytest.fail(f"Failed to import modules: {e}")
